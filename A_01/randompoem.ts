3183780cc8f6ed6aaf47cdbc0c6f47c8bfcee7f6
LuzenkPa <149688203+LuzenkPa@users.noreply.github.com>
2024-03-30 23:15:13 +0100

    ll

--- A_01/randompoem.ts
+++ A_01/randompoem.ts
@@ -1,14 +1,16 @@
-namespace Nicokontrolliert{
+namespace NicoKontrolliert {
 
-    let subject: string []= ["Nico","Jirka","Oxford","Gnar","Jirka"];
-    let predicate: string []= ["kontrolliert","unalived","gapclosed","brummt","liebt"];
-    let object: string []= ["Fahrkarten","Blaupausen","Knochen","Konzepteris","Stühle"];
+    let subject: string[] = ["Nico", "Jirka", "Oxford", "Gnar", "Markus"];
+    let predicate: string[] = ["kontrolliert", "unalived", "gapclosed", "brummt", "liebt"];
+    let object: string[] = ["Fahrkarten", "Blaupausen", "Knochen", "Konzepteris", "Stühle"];
 
-    console.log(subject,predicate,object)
+    console.log(subject, predicate, object)
     for (let i = subject.length; i >= 1; i--) {
         console.log(i)
     }
-    function getVerse(_subject,_predicate,_object)
+    function getVerse(_subject:string, _predicate:string, _object:string){
+        return _subject
     }
+}
 
 
