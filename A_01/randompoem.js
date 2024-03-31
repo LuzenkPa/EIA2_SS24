7d41ed9c65a3e1ca290340fbe912dda272d6cc20
LuzenkPa <149688203+LuzenkPa@users.noreply.github.com>
2024-03-31 03:09:30 +0200

    sdf

--- A_01/randompoem.js
+++ A_01/randompoem.js
@@ -1,12 +1,21 @@
 "use strict";
-var Nicokontrolliert;
-(function (Nicokontrolliert) {
-    let subject = ["Nico", "Jirka", "Oxford", "Gnar", "Jirka"];
-    let predicate = ["kontrolliert", "unalived", "gapclosed", "brummt", "liebt"];
-    let object = ["Fahrkarten", "Blaupausen", "Knochen", "Konzepteris", "Stühle"];
-    console.log(subject, predicate, object);
+var randompoem;
+(function (randompoem) {
+    let subject = ["Nico", "Jirka", "Oxford", "Gnar", "Markus"];
+    let predicate = [" kontrolliert ", " unalived ", " gapclosed ", " brummt ", " liebt "];
+    let object = ["Fahrkarten", "Blaupausen", "La Vida Loca", "Konzepteris", "Stühle"];
+    debugger;
     for (let i = subject.length; i >= 1; i--) {
-        console.log(i);
+        let word = getVerse(subject, predicate, object);
+        console.log(word);
     }
-})(Nicokontrolliert || (Nicokontrolliert = {}));
+    function getVerse(_subject, _predicate, _object) {
+        let sentence = "";
+        let onesubject = Math.floor(Math.random() * _subject.length);
+        let onepredicate = Math.floor(Math.random() * _predicate.length);
+        let oneobject = Math.floor(Math.random() * _object.length);
+        sentence = _subject.splice(oneobject, 1)[0] + _predicate.splice(onepredicate, 1)[0] + _object.splice(oneobject, 1)[0];
+        return sentence;
+    }
+})(randompoem || (randompoem = {}));
 //# sourceMappingURL=randompoem.js.map
--- A_01/randompoem.ts
+++ A_01/randompoem.ts
@@ -1,15 +1,19 @@
-namespace NicoKontrolliert {
-
+namespace randompoem {
     let subject: string[] = ["Nico", "Jirka", "Oxford", "Gnar", "Markus"];
-    let predicate: string[] = ["kontrolliert", "unalived", "gapclosed", "brummt", "liebt"];
-    let object: string[] = ["Fahrkarten", "Blaupausen", "Knochen", "Konzepteris", "Stühle"];
-
-    console.log(subject, predicate, object)
-    for (let i = subject.length; i >= 1; i--) {
-        console.log(i)
+    let predicate: string[] = [" kontrolliert ", " unalived ", " gapclosed ", " brummt ", " liebt "];
+    let object: string[] = ["Fahrkarten", "Blaupausen", "La Vida Loca", "Konzepteris", "Stühle"];
+    debugger;
+    for (let i: number = subject.length; i >= 1; i--) {
+        let word: string = getVerse(subject, predicate, object);
+        console.log(word);
     }
-    function getVerse(_subject:string, _predicate:string, _object:string){
-        return _subject
+    function getVerse(_subject: string[], _predicate: string[], _object: string[]) {
+        let sentence: string = "";
+        let onesubject: number = Math.floor(Math.random() * _subject.length);
+        let onepredicate: number = Math.floor(Math.random() * _predicate.length);
+        let oneobject: number = Math.floor(Math.random() * _object.length);
+        sentence = _subject.splice(oneobject, 1)[0] + _predicate.splice(onepredicate, 1)[0] + _object.splice(oneobject, 1)[0];
+        return sentence;
     }
 }
 
