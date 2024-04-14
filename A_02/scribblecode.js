402d4927c65b098a5a32d828c62d48612aacb135
LuzenkPa <149688203+LuzenkPa@users.noreply.github.com>
2024-04-14 03:57:04 +0200

    sfdga

--- A_02/scribblecode.js
+++ A_02/scribblecode.js
@@ -3,21 +3,31 @@ var scribblecode;
 (function (scribblecode) {
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
-        document.addEventListener("mouse_move", setInfoBox);
+        document.addEventListener("mousemove", setInfoBox);
         document.addEventListener("click", logInfo);
         document.addEventListener("keyup", logInfo);
-        document.getElementsByTagName("div").addEventListener("click", logInfo);
-        document.getElementsByTagName("div").addEventListener("keyup", logInfo);
+        document.getElementById("div0")?.addEventListener("keyup", logInfo);
+        document.getElementById("div0")?.addEventListener("click", logInfo);
+        document.getElementById("div1")?.addEventListener("keyup", logInfo);
+        document.getElementById("div1")?.addEventListener("click", logInfo);
         document.body.addEventListener("click", logInfo);
         document.body.addEventListener("keyup", logInfo);
-        console.log(_event);
     }
     ;
-    function setInfoBox() {
+    function setInfoBox(_event) {
+        let x = _event.clientX;
+        let y = _event.clientY;
+        let span = document.getElementById("span");
+        span.style.top = y + "px";
+        span.style.left = x + "px";
+        span.innerHTML = "Mouseposition " + x + " " + y + _event.target;
     }
     ;
-    function logInfo() {
+    function logInfo(_event) {
+        console.log("type: " + _event.type);
+        console.log("target: " + _event.target);
+        console.log("currentTarget: " + _event.currentTarget);
+        console.log("event: " + _event);
     }
-    ;
 })(scribblecode || (scribblecode = {}));
 //# sourceMappingURL=scribblecode.js.map
--- A_02/scribblecode.ts
+++ A_02/scribblecode.ts
@@ -2,19 +2,29 @@ namespace scribblecode {
     window.addEventListener("load", handleLoad);
 
     function handleLoad(_event: Event): void {
-        document.addEventListener("mouse_move", setInfoBox)
+        document.addEventListener("mousemove", setInfoBox)
         document.addEventListener("click", logInfo)
         document.addEventListener("keyup", logInfo)
-        document.getElementsByTagName("div").addEventListener("click", logInfo)
-        document.getElementsByTagName("div").addEventListener("keyup", logInfo)
+        document.getElementById("div0")?.addEventListener("keyup", logInfo);
+        document.getElementById("div0")?.addEventListener("click", logInfo);
+        document.getElementById("div1")?.addEventListener("keyup", logInfo);
+        document.getElementById("div1")?.addEventListener("click", logInfo);
         document.body.addEventListener("click", logInfo)
         document.body.addEventListener("keyup", logInfo)
-        console.log(_event)
     };
-    function setInfoBox() {
+    function setInfoBox(_event: MouseEvent): void {
+        let x: number = _event.clientX;
+        let y: number = _event.clientY;
+        let span: HTMLElement = <HTMLElement>document.getElementById("span");
+        span.style.top = y + "px";
+        span.style.left = x + "px";
+        span.innerHTML = "Mouseposition " + x + " " + y + _event.target;
 
     };
-    function logInfo() {
-
-    };
+    function logInfo(_event: Event): void {
+        console.log("type: " + _event.type);
+        console.log("target: " + _event.target);
+        console.log("currentTarget: " + _event.currentTarget);
+        console.log("event: " + _event);
+    }
 }
