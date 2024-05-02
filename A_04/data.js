59d143bce2008667ff62db0929df74c4efc443d9
LuzenkPa <149688203+LuzenkPa@users.noreply.github.com>
2024-05-02 15:16:29 +0200

    push

---
+++ A_04/data.js
@@ -0,0 +1,10 @@
+"use strict";
+var Liste;
+(function (Liste) {
+    Liste.data = [
+        { menge: 1.5, einheit: "Liter", name: "Milch", lastbought: new Date("01.01.2024"), bought: true, comment: "haltbare" },
+        { menge: 500, einheit: "Gramm", name: "Brot", lastbought: new Date("01.01.2024"), bought: true, comment: "schwenninger Laib" },
+        { menge: 20, einheit: "Stück", name: "Eier", lastbought: new Date("01.01.2024"), bought: true, comment: "bodenhaltung" }
+    ];
+})(Liste || (Liste = {}));
+//# sourceMappingURL=data.js.map
